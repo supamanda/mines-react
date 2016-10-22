fd1913658e273af05a27b4950f3ddffa7a94b36b
Amanda Helliwell <amandahelliwell@Amandas-MBP.gateway>
2016-10-22 15:59:01 +1100

    Add support for redux dev tools chrome extension

--- src/js/store.js
+++ src/js/store.js
@@ -7,4 +7,7 @@ import reducer from "./reducers"
 
 const middleware = applyMiddleware(thunk, logger())
 
-export default createStore(reducer, middleware)
+// export default createStore(reducer, middleware)
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export default createStore(reducer, composeEnhancers(middleware));
